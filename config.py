916ee5476657eed97b441eccb43c325540d6d2d6
mkjv08 <116807036+mkjv08@users.noreply.github.com>
2022-11-23 19:26:49 +0500

    Add files via upload

---
+++ config.py
@@ -0,0 +1,10 @@
+from aiogram import Bot, Dispatcher
+import logging
+
+BOT_TOKEN = '5625215749:AAHZ_sxcf_1ZMIJ2gRHEJvz9rupsiAeoVZo'
+bot = Bot(token=BOT_TOKEN)
+dp = Dispatcher(bot)
+
+
+def PAYMENTS_TOKEN():
+    return None
